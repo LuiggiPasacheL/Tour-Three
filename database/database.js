d3982483e9a9820eb0245470dd23dee4c509e63b
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2021-12-30 19:23:45 -0500

    agregando conexion a base de datos

--- database/database.js
+++ database/database.js
@@ -1,4 +1,21 @@
 
-const config = require('dotenv').config();
+const util = require('util');
+const mysql = require('mysql');
+require('dotenv').config();
 
+var pool = mysql.createPool({
+  host:      process.env.HOST,
+  database:  process.env.DATABASE,
+  user:      process.env.USERDB,
+  password:  process.env.PASSWORDDB
+});
 
+pool.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+  console.log('Conexion a la base de datos correcta');
+});
+
+const query = util.promisify(pool.query);
+
+module.exports = query;
---
+++ database/tables.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE tourthree;
+
+USE tourthree;
+
+CREATE TABLE users ();
